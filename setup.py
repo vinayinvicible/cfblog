97b3db115a21bcc64cdd8570871aaa8a3189c247
Vinay Karanam <vinayinvicible@gmail.com>
2015-10-08 19:29:49 +0530

    changed version number. added entry in changelog.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     description="Awesome CMS for Django",
     long_description=long_description,
 
-    version="0.1.1",
+    version="0.1.2",
     author='Vinay Karanam',
     author_email="vinayinvicible@gmail.com",
 
