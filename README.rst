0fad2c4ce0d0f4b0fb52ba13b4e07b58d9bbb84f
Amit Upadhyay <amitu@browserstack.com>
2015-10-08 17:39:25 +0530

    release_0_1_1

---
+++ README.rst
@@ -0,0 +1,30 @@
+What is cfblog?
+===============
+
+It is a blog engine with a peculiar CMS feature, that allows very complex
+content to be easily created.
+
+Installation
+============
+
+.. code:: bash
+
+    $ pip install cfblog
+
+
+Contributors
+============
+
+* Amit Upadhyay (https://github.com/amitu)
+* Vinay Karanam (https://github.com/vinayinvicible)
+
+ChangeLog
+=========
+
+https://github.com/Coverfox/cfblog/blob/master/ChangeLog.rst
+
+
+LICENSE
+=======
+
+* BSD
--- setup.py
+++ setup.py
@@ -1,12 +1,28 @@
-from distutils.core import setup
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+try:
+    long_description = open('README.rst', 'rt').read()
+except Exception:
+    long_description = ""
 
 setup(
-    name='cfblog',
-    version='0.1b2',
-    packages=['cfblog', 'cfblog.migrations'],
-    url='https://github.com/vinayinvicible/cfblog/',
-    license='BSD',
+    name="cfblog",
+    description="Awesome CMS for Django",
+    long_description=long_description,
+
+    version="0.1.1",
     author='Vinay Karanam',
-    author_email='vinayinvicible@gmail.com',
-    description=''
+    author_email="vinayinvicible@gmail.com",
+
+    url='https://github.com/Coverfox/cfblog/',
+    license='BSD',
+
+    install_requires=[
+        "fhurl>=0.1.7", "smarturls", "six", "Django>=1.3",
+        "dj-database-url",
+    ],
+    packages=['cfblog', 'cfblog.migrations'],
+
+    zip_safe=True,
 )
