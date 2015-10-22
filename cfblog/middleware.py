baaceb03d74be51b935a3bb5bcdc9846b5a6114b
Vinay Karanam <vinayinvicible@gmail.com>
2015-10-23 00:01:35 +0530

    lazy importing 'cms_page_index` inside middleware.
    This is to let models be imported after app is properly loaded.

--- cfblog/middleware.py
+++ cfblog/middleware.py
@@ -2,8 +2,6 @@ __author__ = 'vinay'
 from django.conf import settings
 from django.http.response import Http404
 
-from .views import cms_page_index
-
 
 class Middleware(object):
 
@@ -18,6 +16,7 @@ class Middleware(object):
         if response.status_code != 404:
             return response
 
+        from .views import cms_page_index
         # we return the original response
         # if the 404 response is given by cms_page_index
         if request.resolver_match and request.resolver_match.func is cms_page_index:
